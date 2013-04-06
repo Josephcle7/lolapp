e5367c9d5f7a5cb4771d25c89c631a05636716c9
Josephcle7 <joseph.clevenger7@yahoo.com>
2013-04-05 17:34:06 -0700

    Calculators

---
+++ twoarmor.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int twoad(char *Boots, char *Special, char *Wards, char *Counter){
+
+	printf("\n%s, %s, %s, %s, 2 armor Items.\n\n", Boots, Special, Wards, Counter);
+
+	return 0;
+}
